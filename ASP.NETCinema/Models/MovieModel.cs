87fc027338fb35561ad6f0f8d0290ccf585a9f2a
Tim Chermin <tim.chermin@student.fontys.nl>
2019-02-17 13:55:07 +0100

    Added MovieModel with prop

---
+++ ASP.NETCinema/Models/MovieModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCinema.Models
+{
+    public class MovieModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime LastScreeningDate { get; set; }
+        public string MovieType { get; set; }
+
+
+
+    }
+}
