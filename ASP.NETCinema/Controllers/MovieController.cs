9d3a13e3175951b2553bf2d7fb0382a201028b06
Tim Chermin <tim.chermin@student.fontys.nl>
2019-02-17 14:55:02 +0100

    Added ListMovies View and added method of ListMovies in MovieController

--- ASP.NETCinema/Controllers/MovieController.cs
+++ ASP.NETCinema/Controllers/MovieController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNETCinema.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPNETCinema.Controllers
@@ -11,6 +12,17 @@ namespace ASPNETCinema.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ListMovies()
+        {
+            //using ASPNETCinema.Models; added
+            List<MovieModel> movies = new List<MovieModel>();
+            
+            movies.Add(new MovieModel(1, "300", "Fightingstuff and thangs", "3D"));
+            movies.Add(new MovieModel(2, "400", "Fightingstuff and thangs", "3D IMAX"));
+            movies.Add(new MovieModel(3, "500", "Fightingstuff and thangs", "IMAX"));
+            return View(movies);
         }
     }
 }
--- ASP.NETCinema/Models/MovieModel.cs
+++ ASP.NETCinema/Models/MovieModel.cs
@@ -7,6 +7,14 @@ namespace ASPNETCinema.Models
 {
     public class MovieModel
     {
+        public MovieModel(int iD, string name, string description, string movieType)
+        {
+            ID = iD;
+            Name = name;
+            Description = description;
+            MovieType = movieType;
+        }
+
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
