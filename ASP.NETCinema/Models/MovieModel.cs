7cec6a34c28d6ff74f59020a0aaeff4f6bcbdd3a
Tim Chermin <tim.chermin@student.fontys.nl>
2019-02-26 13:44:55 +0100

    Added movie lenght to the movielist also started with Required messages in movieModel

--- ASP.NETCinema/Models/MovieModel.cs
+++ ASP.NETCinema/Models/MovieModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASPNETCinema.Models
 {
@@ -33,13 +34,18 @@ namespace ASPNETCinema.Models
         }
 
         
-
+        //[Range(100000, 999999, ErrorMessage = "This ID isn't good")]
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        //[Range(DateTime.Today, DateTime.Today, ErrorMessage = "This ID isn't good")]
         public DateTime ReleaseDate { get; set; }
         public DateTime LastScreeningDate { get; set; }
         public string MovieType { get; set; }
+
+        //[Display(Name = "Movie Lenght")]
+        [Required(ErrorMessage = "You need to fill in the movie lenght")]
         public string MovieLenght { get; set; }
         
 
