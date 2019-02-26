a49c7542fe798bf1212028ceebd262bd70f6f02c
Tim Chermin <tim.chermin@student.fontys.nl>
2019-02-26 13:49:25 +0100

    Added required and display attributes to MovieModel

--- ASP.NETCinema/Models/MovieModel.cs
+++ ASP.NETCinema/Models/MovieModel.cs
@@ -36,15 +36,27 @@ namespace ASPNETCinema.Models
         
         //[Range(100000, 999999, ErrorMessage = "This ID isn't good")]
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "The Name field is required.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "The Description field is required.")]
         public string Description { get; set; }
 
         //[Range(DateTime.Today, DateTime.Today, ErrorMessage = "This ID isn't good")]
+        [Display(Name = "Release date")]
+        [Required(ErrorMessage = "The Release Date field is required.")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Last Screening Date")]
+        [Required(ErrorMessage = "The Last Screening Date field is required.")]
         public DateTime LastScreeningDate { get; set; }
+
+        [Display(Name = "Movie Type")]
+        [Required(ErrorMessage = "The Movie Type field is required.")]
         public string MovieType { get; set; }
 
-        //[Display(Name = "Movie Lenght")]
+        [Display(Name = "Movie Lenght")]
         [Required(ErrorMessage = "You need to fill in the movie lenght")]
         public string MovieLenght { get; set; }
         
