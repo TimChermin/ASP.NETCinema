3972788f5d0144daacc796599eaee8c0dbe71fdc
Tim Chermin <tim.chermin@student.fontys.nl>
2019-03-12 14:56:36 +0100

    Added listTasks

---
+++ ASP.NETCinema/Controllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCinema.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCinema.Controllers
+{
+    public class TaskController : Controller
+    {
+        TaskLogic taskLogic = new TaskLogic();
+
+        public ActionResult ListTasks()
+        {
+            return View(taskLogic.GetTasks());
+        }
+    }
+}
--- ASP.NETCinema/Models/TaskModel.cs
+++ ASP.NETCinema/Models/TaskModel.cs
@@ -7,13 +7,21 @@ namespace ASPNETCinema.Models
 {
     public class TaskModel
     {
+        public TaskModel(int id, int idScreening, int taskType)
+        {
+            Id = id;
+            IdScreening = idScreening;
+            TaskType = taskType;
+        }
+
+
         public TaskModel()
         {
         }
 
 
         public int Id { get; set; }
-        public int ScreeningId { get; set; }
+        public int IdScreening { get; set; }
         public int TaskType { get; set; }
         public ScreeningModel Screening { get; set; }
         public List<EmployeeModel> Employees { get; set; }
