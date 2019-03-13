5de5fd965275d96e97559c1000498e600794d722
Tim Chermin <tim.chermin@student.fontys.nl>
2019-03-13 14:49:28 +0100

    removed using that wasn't used lol

--- LogicLayer/MovieLogic.cs
+++ LogicLayer/MovieLogic.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ASPNETCinema.Models;
-using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 
 namespace ASPNETCinema.Logic
