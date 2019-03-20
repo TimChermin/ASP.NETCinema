103a0f680eecc1cea47e6bb6fb9d7798a9fa9d80
Tim Chermin <tim.chermin@student.fontys.nl>
2019-03-20 21:53:49 +0100

    added movieDto

---
+++ DAL/Dtos/MovieDto.cs
@@ -0,0 +1,19 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Dtos
+{
+    internal class MovieDto : IMovie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set ; }
+        public DateTime LastScreeningDate { get ; set ; }
+        public string MovieType { get ; set ; }
+        public string MovieLenght { get ; set ; }
+        public string ImageString { get ; set ; }
+    }
+}
