4da7a17261c4dfcb898eec11fa549b1df290d3cc
Tim Chermin <timchermin@hotmail.com>
2019-04-26 11:53:20 +0200

    updated the planning, UML and documentation

---
+++ LogicLayer/Interfaces/IMovieLogic.cs
@@ -0,0 +1,18 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Interfaces
+{
+    public interface IMovieLogic
+    {
+        IEnumerable<IMovie> GetMovies(string orderBy);
+        void AddMovie(IMovie movie);
+        IMovie GetMovieById(int id);
+        void EditMovie(IMovie movie);
+        void DeleteMovie(int id);
+
+        IEnumerable<IScreening> GetScreeningsForMovie(int idMovie);
+    }
+}
