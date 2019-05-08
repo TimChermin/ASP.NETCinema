ecc4f2b21c6968311a76abd471ad470b3b02fbb5
Tim Chermin <timchermin@hotmail.com>
2019-05-08 13:31:23 +0200

    triggers and stored added

---
+++ SQLQueryTriggerMovieDelete.sql
@@ -0,0 +1,17 @@
+﻿
+
+--(BEFORE | AFTER)_Movie_(INSERT| UPDATE | DELETE)
+
+
+ 
+
+CREATE TRIGGER before_movie_delete
+ON dbo.Movie
+INSTEAD OF delete
+AS
+BEGIN
+   DELETE FROM Screening WHERE 1002 = Screening.IdMovie;
+   
+   DELETE FROM Movie WHERE Movie.Id = 1002
+END
+
